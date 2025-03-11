b57500f90fab2fdf8d21acb7e951a8ec6d3b0598
ZamasuEQTD <franco2015gaby71@gmail.com>
2025-03-10 21:42:33 -0300

    remplanteando widget

--- lib/shared/presentation/widgets/tag.dart
+++ lib/shared/presentation/widgets/tag.dart
@@ -1,35 +1,46 @@
 import 'package:flutter/material.dart';
 
 class Tag extends StatelessWidget {
-  final Widget child;
-
-  final Color? background;
+  final Widget label;
   final EdgeInsets? padding;
+  final BorderRadius? borderRadius;
+  final Color? color;
 
-  const Tag({super.key, required this.child, this.background, this.padding});
-
-  factory Tag.text(String text, {Color? background, TextStyle? style, EdgeInsets? padding}) =>
-      Tag(
-        background: background,
-        padding: padding,
-        child: Text(text, style: const TextStyle(fontWeight: FontWeight.w600).merge(style)),
-      );
+  const Tag({
+    super.key,
+    required this.label,
+    this.borderRadius,
+    this.color,
+    this.padding,
+  });
+  factory Tag.text(
+    String text, {
+    Key? key,
+    BorderRadius? borderRadius,
+    Color? color,
+    EdgeInsets? padding,
+    TextStyle? style,
+  }) {
+    return Tag(
+      key: key,
+      label: Text(text, style: style),
+      borderRadius: borderRadius,
+      color: color,
+      padding: padding,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    Widget child = this.child;
-
-    if (padding != null) {
-      child = Padding(padding: padding!, child: child);
-    }
-
-    if (background != null) {
-      child = ClipRRect(
-        borderRadius: BorderRadius.all(Radius.circular(6)),
-        child: ColoredBox(color: background!, child: child),
-      );
-    }
-
-    return child;
+    return ClipRRect(
+      borderRadius: borderRadius ?? BorderRadius.circular(5),
+      child: ColoredBox(
+        color: color ?? Colors.white,
+        child: Padding(
+          padding: padding ?? EdgeInsets.symmetric(horizontal: 6, vertical: 4),
+          child: label,
+        ),
+      ),
+    );
   }
 }
