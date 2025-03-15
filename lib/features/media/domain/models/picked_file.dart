aee6aca89641be57713fe794dec54e971147b3f0
ZamasuEQTD <franco2015gaby71@gmail.com>
2025-03-15 19:10:38 -0300

    cambios en picked file

--- lib/features/media/domain/models/picked_file.dart
+++ lib/features/media/domain/models/picked_file.dart
@@ -1,15 +1,36 @@
+import 'package:equatable/equatable.dart';
 import 'package:inkboard/features/media/domain/models/media.dart';
 
-class PickedFile {
+class PickedFile extends Equatable {
   final String source;
 
   final MediaProvider provider;
 
   final String contentType;
 
+  final bool spoiler;
   const PickedFile({
     required this.source,
     required this.provider,
     required this.contentType,
+    this.spoiler = false,
   });
+
+  PickedFile copyWith({
+    String? source,
+    MediaProvider? provider,
+    String? contentType,
+    bool? spoiler,
+  }) {
+    return PickedFile(
+      source: source ?? this.source,
+      provider: provider ?? this.provider,
+      contentType: contentType ?? this.contentType,
+      spoiler: spoiler ?? this.spoiler,
+    );
+  }
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [source];
 }
--- lib/features/media/presentation/widgets/miniatura/picked_media_miniatura.dart
+++ lib/features/media/presentation/widgets/miniatura/picked_media_miniatura.dart
@@ -1,15 +1,22 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get_it/get_it.dart';
 import 'package:inkboard/features/media/domain/iminiatura_service.dart';
 import 'package:inkboard/features/media/domain/models/picked_file.dart';
+import 'package:inkboard/shared/presentation/widgets/effects/blur/blur.dart';
+import 'package:inkboard/shared/presentation/widgets/effects/gradient/gradient_effect.dart';
 
 class PickedMediaMiniatura extends StatefulWidget {
   final PickedFile media;
 
-  const PickedMediaMiniatura({super.key, required this.media});
+  final bool blurear;
+
+  const PickedMediaMiniatura({
+    super.key,
+    required this.media,
+    this.blurear = false,
+  });
 
   @override
   State<PickedMediaMiniatura> createState() => _PickedMediaMiniaturaState();
@@ -33,16 +40,38 @@ class _PickedMediaMiniaturaState extends State<PickedMediaMiniatura> {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
-      child: SizedBox.square(
-        dimension: 80,
-        child: Obx(() {
-          if (miniaturaGenerada) {
-            return Image(image: miniatura.value!, fit: BoxFit.cover);
-          }
+      child: Obx(() {
+        if (miniaturaGenerada) {
+          return ClipRRect(
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            child: Stack(
+              children: [
+                SizedBox.square(
+                  dimension: 80,
+                  child: Image(image: miniatura.value!, fit: BoxFit.cover),
+                ),
+                Positioned.fill(
+                  child: ClipRect(
+                    child: Blur(
+                      blurear: widget.blurear,
+                      child: GradientEffectWidget(
+                        colors: [
+                          Colors.black12,
+                          Colors.transparent,
+                          Colors.black12,
+                        ],
+                        stops: [0.1, 0.5, 1],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
 
-          return CircularProgressIndicator();
-        }),
-      ),
+        return CircularProgressIndicator();
+      }),
     );
   }
 
